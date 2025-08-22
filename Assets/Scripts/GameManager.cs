d6d0913d17bf1c2f58990114805f0db5686b81a2
Tabolrin <93069446+Tabolrin@users.noreply.github.com>
2025-08-22 18:17:08 +0300

    mroee init

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private GameManager insatance;
+    
+    private void Awake()
+    {
+        
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
