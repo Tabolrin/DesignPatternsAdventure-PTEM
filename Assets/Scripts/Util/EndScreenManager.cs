e90e61046b715caeae633be2d528508b36912f9e
Tabolrin <93069446+Tabolrin@users.noreply.github.com>
2025-08-23 01:35:36 +0300

    minor fixes

--- Assets/Scripts/Util/EndScreenManager.cs
+++ Assets/Scripts/Util/EndScreenManager.cs
@@ -7,24 +7,31 @@ using Image = UnityEngine.UI.Image;
 public class EndScreenManager : MonoBehaviour
 {
     [SerializeField] private Image blackOverlay;
-    [SerializeField] private float fadeDuration;
+    [SerializeField] private float fadeDuration = 2;
+
     private void Start()
     {
         blackOverlay.color = Color.black;
-        
-        blackOverlay.CrossFadeColor
-        (
-            new Color(0, 0, 0, 0),
-            fadeDuration,
-            true,
-            true
-        );
+        StartCoroutine(FadeOut());
         StartCoroutine(DelayedDestruction());
     }
 
     public void StartOver()
     {
         SceneManager.LoadScene("GameScene");
+    }
+
+    private IEnumerator FadeOut()
+    {
+        float t = 0f;
+        while (t < fadeDuration)
+        {
+            t += Time.unscaledDeltaTime; 
+            float a = Mathf.Lerp(1f, 0f, t / fadeDuration);
+            blackOverlay.color = new Color(0f, 0f, 0f, a);
+            yield return null;
+        }
+        blackOverlay.color = new Color(0f, 0f, 0f, 0f); 
     }
 
     private IEnumerator DelayedDestruction()
